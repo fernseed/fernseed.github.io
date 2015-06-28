3d1b88cf5058928e2bdab2bf44a87a045091078e
Darren McGuicken <dmcgk@users.noreply.github.com>
2015-06-28 17:02:58 +0100

    Modify responsive sidebar visibility.

--- _includes/js/responsive.js
+++ _includes/js/responsive.js
@@ -1,7 +1,10 @@
 function adjustBehaviourForScreenSize() {
    var width = $(window).width();
-   if(width >= 1380){
+   if(width >= 1366){
        $('body').addClass('sidebar-overlay');
+       if(width >= 1680){
+       		document.querySelector('#sidebar-checkbox').checked = true
+       }
    }
    else{
        $('body').removeClass('sidebar-overlay');
